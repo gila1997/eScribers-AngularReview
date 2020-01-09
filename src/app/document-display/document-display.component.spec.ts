81531128dbae95b2a49116d38fcd6e2a52477e5b
gila1997 <giladadon1997@gmail.com>
2020-01-08 20:58:34 -0800

    add display component

---
+++ src/app/document-display/document-display.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DocumentDisplayComponent } from './document-display.component';
+
+describe('DocumentDisplayComponent', () => {
+  let component: DocumentDisplayComponent;
+  let fixture: ComponentFixture<DocumentDisplayComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ DocumentDisplayComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DocumentDisplayComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/document-display/document-display.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-document-display',
+  templateUrl: './document-display.component.html',
+  styleUrls: ['./document-display.component.css']
+})
+export class DocumentDisplayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
